fd290476b7c0a28f43be8e215b313a836eb194a8
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-04-22 15:36:47 +0300

    Initial Commit

---
+++ AutoLayoutConstraintsProgrammatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutConstraintsProgrammatically
+//
+//  Created by Sergey Lukaschuk on 22.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
