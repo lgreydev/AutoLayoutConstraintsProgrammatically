8a4f6f5446835995d28b412ebc2202da929d3ef5
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-04-22 16:33:09 +0300

    Add second view

--- AutoLayoutConstraintsProgrammatically/ViewController.swift
+++ AutoLayoutConstraintsProgrammatically/ViewController.swift
@@ -9,16 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    private let myView: UIView = {
+    private let blueView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .blue
+        view.backgroundColor = .link
+        return view
+    }()
+
+    private let redView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .red
         return view
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(myView)
+        
         addConstraints()
     }
 
@@ -30,11 +37,21 @@ extension ViewController {
     private func addConstraints() {
         var constraints = [NSLayoutConstraint]()
 
-        constraints.append(myView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor))
-        constraints.append(myView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor))
-        constraints.append(myView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor))
-        constraints.append(myView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor))
+        view.addSubview(blueView)
+        constraints.append(blueView.leadingAnchor.constraint(equalTo: view.leadingAnchor))
+        constraints.append(blueView.trailingAnchor.constraint(equalTo: view.trailingAnchor))
+        constraints.append(blueView.topAnchor.constraint(equalTo: view.topAnchor))
+        constraints.append(blueView.bottomAnchor.constraint(equalTo: view.bottomAnchor))
+
+        view.addSubview(redView)
+        constraints.append(redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100))
+        constraints.append(redView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100))
+        constraints.append(redView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100))
+        constraints.append(redView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100))
 
         NSLayoutConstraint.activate(constraints)
     }
+
+
+
 }
